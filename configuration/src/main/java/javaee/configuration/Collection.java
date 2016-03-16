1c90de4b10bd83c1db709e08fc0daf97c2d9d54a
Sergej Samsonow <sergej.samsonow.public@googlemail.com>
2016-03-16 10:41:00 +0100

    Add more TODO comments

--- configuration/src/main/java/javaee/configuration/Collection.java
+++ configuration/src/main/java/javaee/configuration/Collection.java
@@ -31,6 +31,7 @@ public class Collection {
         return content != null ? Boolean.parseBoolean(content) : null;
     }
 
+    // TODO return not null
     public Boolean bool(String key, Boolean defaultValue) {
         if (contains(key)) {
             return bool(key);
@@ -38,12 +39,17 @@ public class Collection {
         return defaultValue;
     }
 
+    // TODO return value or boolean true
+    // boolOrTrue(String key);
+    // boolOrFalse(String key);
+
     // TODO return null and trigger cdi event on parse exception
     public Integer integer(String key) {
         String content = data.get(key);
         return content != null ? Integer.parseInt(content) : null;
     }
 
+    // TODO return not null
     public Integer integer(String key, Integer defaultValue) {
         if (contains(key)) {
             return integer(key);
@@ -55,6 +61,7 @@ public class Collection {
         return data.get(key);
     }
 
+    // TODO return not null
     public String str(String key, String defaultValue) {
         if (contains(key)) {
             return str(key);
